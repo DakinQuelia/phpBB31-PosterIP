b04ea4b7563e6652938eca3c36d6ee8da8d8e0e0
DakinQuelia <dakinquelia@gmail.com>
2015-03-23 21:56:25 +0100

    First release
    First release of extension

---
+++ README.md
@@ -0,0 +1,23 @@
+# Poster IP extension for phpBB 3.1
+
+This extension allows Moderator and Administrator to View the IP of the Poster on Viewtopic.
+
+## Requirements
+* phpBB 3.1.3 or higher
+* PHP 5.3.3 or higher
+
+## Installation
+1. Download the repository ZIP and unzip it.
+2. Copy the entire contents from the unzipped folder to `phpBB/ext/dakinquelia/posterip/`.
+3. Navigate in the ACP to `Customise -> Manage extensions`.
+4. Find Poster IP under "Disabled Extensions" and click `Enable`.
+
+## Uninstallation
+1. Navigate in the ACP to `Customise -> Manage extensions`.
+2. Click the `Disable` link for Poster IP.
+3. To permanently uninstall, click `Delete Data`, then delete the `posterip` folder from `phpBB/ext/dakinquelia/`.
+
+## License
+[GNU General Public License v2](http://opensource.org/licenses/GPL-2.0)
+
+© 2015 - Daniel Chalsèche (Dakin Quelia)
---
+++ event/listener.php
@@ -0,0 +1,59 @@
+<?php
+/**
+*
+* @package phpBB Extension - Poster IP
+* @copyright (c) 2015 Dakin Quelia
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace dakinquelia\posterip\event;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+* Event listener
+*/
+class listener implements EventSubscriberInterface
+{
+	protected $auth;
+	protected $user;
+	protected $db;
+	protected $phpbb_root_path;
+	protected $php_ext;
+	
+	static public function getSubscribedEvents()
+    {
+        return array(
+            'core.viewtopic_modify_post_row'				=> 'display_posterip_viewtopic',
+        );
+    }
+	
+	/**
+	* Instead of using "global $user;" in the function, we use dependencies again.
+	*/
+	public function __construct(\phpbb\db\driver\driver_interface $db, \phpbb\auth\auth $auth, \phpbb\user $user, $phpbb_root_path, $php_ext)
+	{
+		$this->db = $db;
+		$this->auth = $auth;
+		$this->user = $user;
+		$this->root_path = $phpbb_root_path;
+		$this->php_ext = $php_ext;
+	}
+    
+    public function display_posterip_viewtopic($event)
+    {		
+		$row = $event['row'];
+		$sql = 'SELECT poster_ip FROM ' . POSTS_TABLE . '
+				WHERE post_id = ' . (int) $row['post_id'];
+		$result = $this->db->sql_query($sql);
+		$poster_ip = $this->db->sql_fetchfield('poster_ip');
+		$this->db->sql_freeresult($result);
+		
+		$event['post_row'] = array_merge($event['post_row'], array(
+			'POSTER_IP' 		=> $poster_ip,
+			'POSTER_IP_VISIBLE' => ($this->auth->acl_get('a_') || $this->auth->acl_get('m_')) ? true : false,
+			'POSTER_IP_WHOIS'	=> "http://en.utrace.de/?query=" . $poster_ip,
+		));
+    }
+}
